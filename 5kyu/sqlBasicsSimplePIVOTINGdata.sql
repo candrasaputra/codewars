bd6eebbf1a673d9089b19ec8aee307fb8e2a555a
candra saputra <candrasaputra@live.com>
2019-07-23 09:54:12 +0700

    SQL Basics: Simple PIVOTING data

--- 5kyu/sqlBasicsSimplePIVOTINGdata.sql
+++ 5kyu/sqlBasicsSimplePIVOTINGdata.sql
@@ -2,10 +2,11 @@ CREATE EXTENSION tablefunc;
 
 SELECT *
 FROM  crosstab(
-      'SELECT
-        p.name, detail, COUNT(d.id)
-      FROM products p
-      JOIN details d ON d.product_id = p.id
-      GROUP BY p.id, d.detail
-      ORDER BY 1,2')
-AS ct (name text, bad bigint, good bigint, ok bigint)
+  'SELECT
+      p.name, detail, COUNT(d.id)
+  FROM products p
+  JOIN details d ON d.product_id = p.id
+  GROUP BY p.id, d.detail
+  ORDER BY 1,2',
+  $$values ('good'), ('ok'), ('bad')$$)
+AS ct (name text, good int, ok int, bad int)
